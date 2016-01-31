cedcc18d1aa7e74f0dd0c0e839bdc8876ff86c4b
Alex Roisenberg <alexrois@users.noreply.github.com>
2016-01-31 21:29:17 +0000

    Create basic classes and tests

--- lib/rbtime.rb
+++ lib/rbtime.rb
@@ -7,5 +7,16 @@ module Rbtime
     def time_now
       return Time.now
     end
+    
+    def time_year
+      t = Time.now
+      return t.year
+    end
+  end
+  
+  class Past
+  end
+  
+  class Future
   end
 end
--- test/test_rbtime.rb
+++ test/test_rbtime.rb
@@ -2,14 +2,18 @@ require "./lib/rbtime.rb"
 require "test/unit"
 
 class TestRbtime < Test::Unit::TestCase
+  
   def test_that_it_has_a_version_number
     refute_nil ::Rbtime::VERSION
   end
   
-  def test_time_now
+  def test_current_time_methods
     i = Rbtime::Current.new
-    x = i.time_now
-    y = Time.now
-    assert_equal(x.to_s, y.to_s)
+    x1 = i.time_now
+    y1 = Time.now
+    assert_equal(x1.to_s, y1.to_s)
+    x2 = i.time_year
+    y2 = Time.now.year
+    assert_equal(x2, y2)
   end
 end
